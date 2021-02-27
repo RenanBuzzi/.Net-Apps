4a990a5dd44a6c822ee13d0a99f4d6b951aac9da
RenanBuzzi <renangasparbuzzi@gmail.com>
2021-02-27 18:35:19 +0000

    Full Login project was done on .Net Core 3.1 using MVC.

---
+++ LoginRegistrationUsingIdentity/MVCLoginAndRegistrationUsingIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCLoginAndRegistrationUsingIdentity.Areas.Identity.Data;
+using MVCLoginAndRegistrationUsingIdentity.Data;
+
+[assembly: HostingStartup(typeof(MVCLoginAndRegistrationUsingIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace MVCLoginAndRegistrationUsingIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => 
+                {
+
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
